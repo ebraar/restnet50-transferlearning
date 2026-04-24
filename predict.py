6e869458ce79f21502b9a7c3dc2302b88a2f046f
ebraar <ebbet468@gmail.com>
2026-04-24 15:14:26 +0300

    optimize predict warmup

--- predict.py
+++ predict.py
@@ -1,9 +1,14 @@
+import os
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+
 import numpy as np
 import tensorflow as tf
 from PIL import Image
 from tensorflow.keras import layers, models
 from tensorflow.keras.applications import ResNet50
 from tensorflow.keras.applications.resnet50 import preprocess_input
+
+tf.get_logger().setLevel("ERROR")
 
 IMG_SIZE = (224, 224)
 WEIGHTS_PATH = "models/resnet50_flower.weights.h5"
@@ -43,13 +48,17 @@ def build_model():
 model = build_model()
 model.load_weights(WEIGHTS_PATH)
 
+# Model warm-up: ilk istekte 502/timeout riskini azaltır
+dummy_input = np.zeros((1, 224, 224, 3), dtype=np.float32)
+model.predict(dummy_input, verbose=0)
+
 
 def predict_image(img_path):
     img = Image.open(img_path).convert("RGB").resize(IMG_SIZE)
-    x = np.array(img)
+    x = np.array(img, dtype=np.float32)
     x = np.expand_dims(x, axis=0)
 
-    preds = model.predict(x)[0]
+    preds = model.predict(x, verbose=0)[0]
     top_idx = int(np.argmax(preds))
 
     return {
